dfa573cf9f7b7144ff32cd8b2790d18d9c57b2d2
Adimachukwu Benjamin <thegreatben27@gmail.com>
2023-12-14 23:12:01 +0100

    my-portfolio

---
+++ src/assets/images/index.js
@@ -0,0 +1,21 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './starbucks.png'
+import tesla from './tesla.png'
+import logo from './logo.svg'
+import pericius from './pericius.png'
+import pericius2 from './pericius2.jpeg'
+import era2 from './era2.png'
+
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    logo,
+    pericius,
+    pericius2,
+    era2
+}
---
+++ src/constants/index.js
@@ -0,0 +1,211 @@
+import { meta, shopify, starbucks, tesla, pericius, pericius2, era2 } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    pricewise,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript,
+    food,
+    cash,
+    java
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    // {
+    //     imageUrl: java,
+    //     name: "java",
+    //     type: "Backend",
+    // },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    // {
+    //     imageUrl: motion,
+    //     name: "Motion",
+    //     type: "Animation",
+    // },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    // {
+    //     imageUrl: nodejs,
+    //     name: "Node.js",
+    //     type: "Backend",
+    // },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Frontend Developer",
+        company_name: "Pericius Technologies",
+        icon: pericius2,
+        iconBg: "#accbe1",
+        date: "March 2023 - Present",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Demonstrated exceptional problem-solving abilities by proactively identifying and resolving numerous bugs and issues, ensuring the seamless operation and stability of the web and mobile products.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Frontend Developer",
+        company_name: "Emergency Response Africa",
+        icon: era2,
+        iconBg: "#fbc3bc",
+        date: "Feb 2021 - March 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Contributed to designing and developing the company's web product using Next.js, React, and TailwindCSS.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/YourGitHubUsername',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+    }
+];
+
+export const projects = [
+    
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Full Stack Threads Clone',
+        description: 'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
+        link: 'https://github.com/BenjaminAdimachukwu/threads',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'snapGram',
+        description: 'Built a photo sharing App to capture lovely personal and family moments, allowing users to share photos and connect with friends in a familiar social media environment.',
+        link: 'https://github.com/BenjaminAdimachukwu/Snapgram',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'CarHub',
+        description: 'Designed and built a web app for finding and comparing cars on the market, streamlining the car-buying process.',
+        link: 'https://github.com/BenjaminAdimachukwu/carHub',
+    },
+    
+    {
+        iconUrl: cash,
+        theme: 'btn-back-red',
+        name: 'Airtime-To-Cash',
+        description: 'Airtime to Cash is a powerful application built with React, TypeScript, Redux, Styled Components, and Node.js for the backend. This app provides a seamless solution for converting excess customer airtime to cash.',
+        link: 'https://github.com/BenjaminAdimachukwu/Airtime-to-cash-Front-End',
+    },
+    {
+        iconUrl: food,
+        theme: 'btn-back-yellow',
+        name: 'Omni-food',
+        description: 'Omni-Food is a convenient food ordering website designed for busy working-class individuals who barely have enough time to prepare their own meals. This application is built with HTML, CSS, and JavaScript to provide a seamless and efficient experience for ordering delicious meals.',
+        link: 'https://github.com/BenjaminAdimachukwu/Omnifood-project',
+    },
+    // {
+    //     iconUrl: food,
+    //     theme: 'btn-back-blue',
+    //     name: 'Omni-food',
+    //     description: 'Omni-Food is a convenient food ordering website designed for busy working-class individuals who barely have enough time to prepare their own meals. This application is built with HTML, CSS, and JavaScript to provide a seamless and efficient experience for ordering delicious meals.',
+    //     link: 'https://github.com/BenjaminAdimachukwu/Omnifood-project',
+    // }
+
+];
